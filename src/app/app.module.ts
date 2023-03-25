f8710899aab5d3836b5b669f9cf2faa8840df221
daniel <davillera77@gmail.com>
2023-03-25 17:17:33 -0500

    added new components and icon pack

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+
 import { AppComponent } from './app.component';
 import { SearchComponent } from './components/search/search.component';
+import { WeatherComponent } from './components/weather/weather.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
-    SearchComponent
+    SearchComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -7,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class SearchComponent {
 
+  faSearch = faSearch;
 }
